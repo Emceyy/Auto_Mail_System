e9c3dbd324c66019c41ef821e0b4ca4016a57dd2
Emre Ceylan Uysal <emceyuysal@gmail.com>
2024-07-26 18:59:52 +0300

    added cooment lines

--- mail.py
+++ mail.py
@@ -1,12 +1,16 @@
-import smtplib
-import getpass
+import smtplib # açılımı: Simple Mail Transfer Protocol
+
+# mail oluşturmak ve düzenlemek için kullandığımız kütüpahaneler:
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+# .env dosyasından çekmek için kullandığımız kütüphaneler:
 import os
 from dotenv import load_dotenv
 
 def send_email():
-    load_dotenv()
+
+    load_dotenv() # env dosyasındaki verilerimizi çağırıyoruz.
 
     sender_password = os.getenv("SENDER_PASSWORD")
     sender_email = os.getenv("SENDER_EMAIL")
@@ -14,7 +18,7 @@ def send_email():
     subject = "Scada Aşım Bilgisi"
 
     while True:
-        user_input = input("Değer Giriniz (q to quit): ")
+        user_input = input("Değer Giriniz (Çıkmak için 'q' ya basınız): ")
         if user_input.lower() == "q":
             break
 
@@ -23,15 +27,18 @@ def send_email():
             base_value = int(os.getenv("BASE_VALUE"))
 
             if Scada_Value >= 0.8 * base_value:
-                body = f"Scada Değeri Aşıldı: {Scada_Value}"
+                # girilen değer base değerin yüzde seksenini aştığı takdirde mail gönderilecek.
+                body = f"Scada Değeri Aşıldı\nBase Değer: {base_value}\nGirilen Değer: {Scada_Value}"
 
                 smtp_server = "smtp.gmail.com"
                 smtp_port = 587
 
-                with smtplib.SMTP(smtp_server, smtp_port) as server:
+                with smtplib.SMTP(smtp_server, smtp_port) as server: # with ile işlem bittikten sonra SMTP otomatik olarak sonlanır.
+
                     server.starttls()
                     server.login(sender_email, sender_password)
 
+                    # mail oluşturuluyor:
                     msg = MIMEMultipart("alternative")
                     msg["Subject"] = subject
                     msg["From"] = sender_email
